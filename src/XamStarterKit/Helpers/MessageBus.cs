f3aef0c8be3be49357298fb528aaf992aae7d739
mrxten <mr.xten@ya.ru>
2018-03-01 17:33:08 +0300

    fix messagebus

--- src/XamStarterKit/Helpers/MessageBus.cs
+++ src/XamStarterKit/Helpers/MessageBus.cs
@@ -23,30 +23,30 @@ namespace XamStarterKit.Helpers
 			MessagingCenter.Send(Instance, message, args);
 		}
 
-		public static void Subscribe(string message, Action callback)
+		public static void Subscribe(object subscriber, string message, Action callback)
 		{
-			MessagingCenter.Subscribe<MessageBus>(Instance, message, (bus) =>
+			MessagingCenter.Subscribe<MessageBus>(subscriber, message, (bus) =>
 			{
 				callback?.Invoke();
 			});
 		}
 
-		public static void Subscribe<TArgs>(string message, Action<TArgs> callback)
+		public static void Subscribe<TArgs>(object subscriber, string message, Action<TArgs> callback)
 		{
-			MessagingCenter.Subscribe<MessageBus, TArgs>(Instance, message, (bus, args) =>
+			MessagingCenter.Subscribe<MessageBus, TArgs>(subscriber, message, (bus, args) =>
 			{
 				callback?.Invoke(args);
 			});
 		}
 
-		public static void Unsubscribe(string message)
+		public static void Unsubscribe(object subscriber, string message)
 		{
-			MessagingCenter.Unsubscribe<MessageBus>(Instance, message);
+			MessagingCenter.Unsubscribe<MessageBus>(subscriber, message);
 		}
 
-		public static void Unsubscribe<TArgs>(string message)
+		public static void Unsubscribe<TArgs>(object subscriber, string message)
 		{
-			MessagingCenter.Unsubscribe<MessageBus, TArgs>(Instance, message);
+			MessagingCenter.Unsubscribe<MessageBus, TArgs>(subscriber, message);
 		}
 	}
 }
